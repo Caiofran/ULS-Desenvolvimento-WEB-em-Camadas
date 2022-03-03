50c72f05523741c9643b9bceba83c6e82bed3cbf
Caio França <42479089+Caiofran@users.noreply.github.com>
2022-03-03 13:49:40 -0300

    att

---
+++ calculadora/src/control/Calculadora.java
@@ -0,0 +1,32 @@
+package control;
+
+public class Calculadora {
+	
+	/*public Calculadora(double valor1, double valor2) {
+		
+	}*/
+	
+	//metodo
+	public double somar(double valor1, double valor2) {
+		/*float x1 = valor1;
+		float x2 = valor2;
+		double soma;*/
+		
+		//soma = x1 + x2;
+		//soma = valor1 + valor2;
+		//System.out.println(soma);
+		return valor1 + valor2;
+	}
+	
+	public double subtrair(double valor1, double valor2) {
+		return valor1 - valor2;
+	}
+	
+	public double dividir(double valor1, double valor2) {
+		return valor1 / valor2;
+	}
+	
+	public double multiplicar(double valor1, double valor2) {
+		return valor1 * valor2;
+	}
+}
